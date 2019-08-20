c2af2f2f8fd24c2b04ec5e7e73dc1fdf714ee899
rodrigogomez14 <rodrigogomez548@gmail.com>
2019-08-20 01:42:29 -0300

    Updates

---
+++ precache-manifest.f096bbe49a22e0dabcae8f21064a5d26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d351ed40a39b029dafe4ec0a0f19e0",
+    "url": "/AnotadUniversalReact/index.html"
+  },
+  {
+    "revision": "aabac9ce80759ad8e053",
+    "url": "/AnotadUniversalReact/static/css/2.568101d1.chunk.css"
+  },
+  {
+    "revision": "600a99e1e7981f834aab",
+    "url": "/AnotadUniversalReact/static/css/main.4377fc21.chunk.css"
+  },
+  {
+    "revision": "aabac9ce80759ad8e053",
+    "url": "/AnotadUniversalReact/static/js/2.15b84ccc.chunk.js"
+  },
+  {
+    "revision": "600a99e1e7981f834aab",
+    "url": "/AnotadUniversalReact/static/js/main.4442c9b9.chunk.js"
+  },
+  {
+    "revision": "d702733c00aa9bd18e54",
+    "url": "/AnotadUniversalReact/static/js/runtime~main.8d063869.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AnotadUniversalReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
